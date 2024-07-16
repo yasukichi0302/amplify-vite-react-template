90a0a17eb4ee4d00fe64f1034b1c2a16dd68be1a
Norihiko Nagayasu <58456149+yasukichi0302@users.noreply.github.com>
2024-07-17 00:17:53 +0900

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -11,7 +11,7 @@ const schema = a.schema({
     .model({
       content: a.string(),
     })
-    .authorization((allow) => [allow.publicApiKey()]),
+    .authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -20,6 +20,7 @@ export const data = defineData({
   schema,
   authorizationModes: {
     defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: 'userPool',
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
