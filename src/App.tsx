4b998ab4388b60cb1ebb6f7416e53a285650b90f
Norihiko Nagayasu <58456149+yasukichi0302@users.noreply.github.com>
2024-07-16 23:41:33 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,10 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   return (
     <main>
